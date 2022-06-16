0274b82c0bb910252efccdd8e0de4d1028473a2c
eduardopcjunior <eduardojunior1402@hotmail.com>
2022-06-16 20:42:24 -0300

    Ajustes contato e index

--- contatoinscricoes.php
+++ contatoinscricoes.php
@@ -1,7 +1,7 @@
 <?php require "includes/cabecalho.php"?>
-    <main>
+    
         <article class="conteudo limitador">
-            <h2>Contato</h2>
+            <h3>Contato</h3>
 
                 <p>Nós, do Projeto Musical Talentos agradecemos seu contato ou seu interesse em fazer parte do nosso time de estudos.</p>
             
@@ -11,59 +11,70 @@
                 <!-- Action - Envio ao formspree.io (envio de dados para o email cadastrado no site) -->
                 <form id="my-form" action="https://formspree.io/f/mwkyrzgj" method="post">
                     
-                    <div>
+                    <div id="ne">
+                        <div>
                         <!-- instrução do caixa (Nome:)  -->
                         <!-- for será igual id -->
                         <label for="nome">Nome:</label>
                         
                         <!-- caixa de entrada -->
                         <!-- required, não permite envio em branco do item -->
+                        
                         <input type="text" name="nome" id="nome" required>
-                    </div>
+                        </div>
 
-                    <div>
                         <!-- instrução do caixa (email:)  -->
                         <!-- for será igual id -->
+                        <div>
                         <label for="email">E-mail:</label>
                         
+
                         <!-- caixa de entrada -->
                         <!-- required, não permite envio em branco do item -->
+                        
                         <input type="email" name="email" id="email" required>
+                        </div>
                     </div>
 
-                    <div>
+                    <div id="dat">
                         <!-- instrução do caixa (Data de nascimento:)  -->
                         <!-- for será igual id -->
+                        <div>
                         <label for="nascimento">Data de nascimento:</label>
+                        </div>
                         
                         <!-- caixa de entrada -->
+                        <div>
                         <input type="date" name="nascimento" id="nascimento">
+                        </div>
 
-                    </div>
-
-                    <div>
                         <!-- instrução do caixa (Idade:)  -->
                         <!-- for será igual id -->
                         <label for="idade">Idade:</label>
                         
                         <!-- caixa de entrada -->
                         <input type="number" name="idade" id="idade" min="18" max="100" placeholder="Idade" disabled>
+                    </div>
 
                             <p> <b> Obs.: Abaixo de 18 anos, será necessário a presença dos Pais ou Responsáveis para fazer inscrição no curso. </b> </p>
 
                     </div>
 
-                    <div>
+                    <div id="telefone">
                         <!-- instrução do caixa (Telefone:)  -->
                         <!-- for será igual id -->
+                        
                         <label for="telefone">Telefone:</label>
                         
+                        
                         <!-- caixa de entrada -->
+                        <div class="tel">
                         <input type="tel" name="telefone" id="telefone">
+                        </div>
                     </div>
 
                     <div class="sexo">
-                        <p>Sexo:</p>
+                        <label>Sexo:</label>
                         
                         <!-- Em input tipo radio, será: -->
                         <!-- Em input, no campo name="sexo" - será igual para masculino e feminino -->
@@ -80,7 +91,7 @@
                     </div>
 
                     <div class="interesses">
-                        <p>Interesses:</p>
+                        <label>Interesses:</label>
 
                         <!-- Em input tipo checkbox, será: -->
                         <!-- Em input, no campo name="interesses" - será igual para design, marketing e programacao -->
@@ -89,6 +100,7 @@
                             <!-- for será igual id -->
                             <!-- javascript não consegue ler type radio e type checkbox, devemos acrescentar o value=""-->
 
+                        <div id="interesses">
                         <input value="Violino" type="checkbox" name="interesses" id="Violino">
                         <label for="Violino">Violino</label>
 
@@ -112,31 +124,45 @@
 
                         <input value="Escaleta" type="checkbox" name="interesses" id="Escaleta">
                         <label for="Escaleta">Escaleta</label>
+                        </div>
 
                     </div>
 
-                    
+                    <div id="formulario">
                     <!-- INÍCIO HTML PARA CEP/ENDEREÇO -->
                     <div>
+                        <div>
                         <label for="cep">CEP:</label>
+                        </div>
+
+                        <div>
                         <input type="text" id="cep" name="cep" maxlength="9" required>
                         <button id="localizar-cep">Buscar</button>
+                        </div>
                         <b id="status"></b>
                     </div>
                     <div>
                         <label for="endereco">Endereço:</label>
+                    </div>
+                    <div>
                         <input type="text" id="endereco" name="endereco" size="30">
                     </div>
                     <div>
                         <label for="bairro">Bairro:</label>
+                    </div>
+                    <div>
                         <input type="text" id="bairro" name="bairro">
                     </div>
                     <div>
                         <label for="cidade">Cidade:</label>
+                    </div>
+                    <div>
                         <input type="text" id="cidade" name="cidade">
                     </div>
                     <div>
                         <label for="estado">Estado:</label>
+                    <div>
+                        <div>
                         <input type="text" id="estado" name="estado">
                     </div>
                     <!-- /FIM HTML PARA CEP/ENDEREÇO -->
@@ -144,7 +170,9 @@
 
                     <div>
                         <!-- Em select, abre caixa de opções -->
+                        <div>
                         <label for="assunto">Assunto desejado:</label>
+                        <div>
                         <select name="assunto" id="assunto">
                             <option></option>
                             <option>Dúvidas</option>
@@ -160,12 +188,13 @@
                     <label for="mensagem">Mensagem: 
                         <span id="maximo"> (restam <b id="caracteres">1000</b> caracteres)</span>
                     </label> <br>
-                    <textarea maxlength="1000" name="mensagem" id="mensagem" cols="20" rows="5"></textarea>
+                    <textarea maxlength="1000" name="mensagem" id="mensagem" cols="40" rows="10"></textarea>
                 </div>
 
                 <button type="submit">Enviar dados</button>
 
                 <p id="my-form-status"></p>
+                </div>
 
                 <!-- Pode ser assim também: -->
                 <!-- <input type="button" value="Enviar dados"> -->
@@ -174,7 +203,7 @@
         
         </article>
         
-    </main>
+    
     
     <?php require "includes/rodape.php" ?>
    
--- noticias.php
+++ noticias.php
@@ -1,51 +1,41 @@
 <?php require "includes/cabecalho.php"?>
 
-<article class="noticias">
+<article class="noticias limitador">
            
-            <h2>Notícias</h2>
+            <h3>Notícias</h3>
 
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero dolorum, maxime et consequatur architecto perspiciatis impedit ullam. Facilis sapiente magni accusantium consectetur qui omnis, consequatur autem iusto sunt tenetur id!</p>
             <p>Ea possimus aut facilis incidunt natus dolorem accusantium impedit expedita cupiditate voluptatum doloremque, sint esse voluptatem voluptate quasi fugit minus eos. Perspiciatis, excepturi ipsam fuga asperiores ducimus dolores quam. Tempore!</p>
             
-           <section class="blocos-noticias">
+           <section class="bnot">
 
                
-            <figure>
+            <figure class="bnot1">
                 <a href="imagens/instrumentos/instrumento musical - violino.jpg">
                     <img class="violino-img" src="imagens/instrumentos/instrumento musical - violino.jpg" alt="violino">
                 </a>
-               <h2>Violino</h2>
+               <h3>Violino</h3>
                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta, ea. Soluta omnis nulla molestiae pariatur.</p>
                 
-            </figure>
-         
-                
-            <figure>
+            
                 <a href="imagens/instrumentos/instrumento musica - violão.jpg">
                     <img src="imagens/instrumentos/instrumento musica - violão.jpg" alt="violão">
                 </a>
-                <h2>Violão</h2>
+                <h3>Violão</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta, ea. Soluta omnis nulla molestiae pariatur.</p>
-            </figure>           
-
             
-            <figure>
                 
                 <a href="instrumentos/instrumentos/imagens/instrumento musical - teclado.jpg">
                     <img src="imagens/instrumentos/instrumento musical - teclado.jpg" alt="instrumento musical teclado">
                 </a>
-                <h2>Teclado</h2>
+                <h3>Teclado</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta, ea. Soluta omnis nulla molestiae pariatur.</p>
             
-            </figure>
-        
-
             
-            <figure> 
                 <a href="imagens/instrumentos/instrumento musical - viola de arco.jpg">
                     <img src="imagens/instrumentos/instrumento musical - viola de arco.jpg" alt="viola de arco">
                 </a>
-                <h2>Viola de Arco</h2>
+                <h3>Viola de Arco</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta, ea. Soluta omnis nulla molestiae pariatur.</p>
                       
             </figure>
@@ -54,7 +44,7 @@
         </section>
         </article> 
     
-        <h2>Nossos apoiadores</h2>
+        <h3>Nossos apoiadores</h3>
 
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero dolorum, maxime et consequatur architecto perspiciatis impedit ullam. Facilis sapiente magni accusantium consectetur qui omnis, consequatur autem iusto sunt tenetur id!</p>
 
@@ -94,7 +84,7 @@
 
 </section>
 </article> 
-<h2><strong>Seu apoio é muito importante para esse projeto acontecer. Clique aqui para nos apoiar.</strong></h2>
+<h3><strong>Seu apoio é muito importante para esse projeto acontecer. Clique aqui para nos apoiar.</strong></h3>
     
 
 <?php require "includes/rodape.php" ?>
