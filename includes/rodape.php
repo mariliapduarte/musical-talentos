e746a7d2ecaf6bab542c922ffac38a1798c5f109
Marilia <mariliapduarte@gmail.com>
2022-06-21 15:46:58 -0300

    ajuste no endereço do rodapé

--- includes/rodape.php
+++ includes/rodape.php
@@ -3,7 +3,7 @@
 <a href="index.php"><img src="imagens/rodape/logo-musical-talentos.jpg" alt="" width="85" height="85"></a>
         <!-- Endereço do projeto Musical Talentos -->
         <address>
-            <p><b>Projeto Musical Talentos</b><br>Escola Estadual José Candido de Souza<br>Rua Diana, 1070 - Pompéia,São Paulo/SP<br>Cep: 05019-000</p>
+            <p><b>Projeto Musical Talentos</b><br>Escola Estadual José Candido de Souza<br>Rua Diana, 1070 - Pompéia, São Paulo/SP<br>Cep: 05019-000</p>
         </address>
 
     </footer>
