3e7096dc1036200cd2376363460e23318b893e3d
mac-ceci10 <escolaeducativa.mac@gmail.com>
2022-06-21 13:46:57 -0300

    Update cabecalho.php
    mudei o título para testar

--- includes/cabecalho.php
+++ includes/cabecalho.php
@@ -26,7 +26,7 @@ switch ($pagina) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= $titulo ?></title>
+    <title><?= $titulo ?> 2022</title>
 
     <!-- Atalho CTRL+clicar no link, cria pasta e arquivo automaticamente -->
     <link rel="stylesheet" href="css/style.css">
